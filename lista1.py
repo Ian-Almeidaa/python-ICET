f6c0a31290c878ec027b0eb32ef4ca768e504710
Ian-Almeidaa <164452386+Ian-Almeidaa@users.noreply.github.com>
2024-10-17 17:31:24 -0400

    lista1.py
    Lista finalizada

--- lista1.py
+++ lista1.py
@@ -20,20 +20,20 @@ def q3():
         print(f'\n{cont}°')
         cont+=1
 
-def q4(numero):
-    if numero < 2:
-        return False   
-    for i in range(2, int(numero**0.5) + 1):
-        if numero % i == 0:
-            return False
-    
-    return True
-numero = int(input("Digite um número: "))
-if eh_primo(numero):
-    print(f"{numero} é um número primo.")
-else:
-    print(f"{numero} não é um número primo.")
+def q4():
+    num = int(input("Digite um número: "))
+    if num <= 1:
+        teste = False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print(f"{num} não é primo.")
+            break
+        print(f"{num} é primo.")
 
+    """if teste == True:
+        print(f"{num} é primo.")
+    else:
+        print(f"{num} não é primo.")"""
 
 def q5():
     num1 = int(input('Digite um número: '))
@@ -46,6 +46,100 @@ def q5():
         print(f'ambos os valores ({num1},{num2}) são iguais')
     
 def q6():
+    num1 = float(input("Digite o primeiro valor: "))
+    num2 = float(input("Digite o segundo valor: "))
+    num3 = float(input("Digite o terceiro valor: "))
+    if num1 > num2 and num1 > num3:
+        print(f'O maior valor é {num1}')
+    elif num2 > num1 and num2 > num3:
+        print(f'O maior valor é {num2}')
+    elif num3 > num1 and num3 > num2:
+        print(f'O maior valor é {num3}')
+
+def q7():
+    num1 = float(input("Digite o primeiro valor: "))
+    num2 = float(input("Digite o segundo valor: "))
+    num3 = float(input("Digite o terceiro valor: "))
+    maior = 0
+    menor = 0
+    if num1 > num2 and num1 > num3:
+        maior = num1
+    elif num2 > num1 and num2 > num3:
+        maior = num2
+    elif num3 > num1 and num3 > num2:
+        maior = num3
+
+    if num1 < num2 and num1 < num3:
+        menor = num1
+    elif num2 < num1 and num2 < num3:
+        menor = num2
+    elif num3 < num1 and num3 < num2:
+        menor = num3
+
+    print(f'a diferença entre o maior número ({maior}) e o menor número ({menor}) é de {maior-menor}')    
+
+def q8():
+    base = float('-inf')
+    soma = 0
+    quantidade = 0
+
+    while True:
+        numNew = float(input('Digite um número: '))
+        if numNew < base:
+            break
+
+        soma += numNew
+        quantidade += 1
+        base = numNew
+
+    media = soma / quantidade
+
+    print(f'Ao total foram digitados {quantidade} números, a média deles foram {media} e a soma total foi de {soma}')
+
+def q9():
+    cont = int(input('Digite quantos números serão comparados: '))
+    maior = float(input('Digite o 1° número: '))
+    for i in range(2,cont + 1):
+        num = float(input(f'Digite o {cont}° número: '))
+        if num > maior:
+            maior = num
     
+    print(f'O maior número entre os {cont} números foi {maior}')
 
+def q10():
+    worldA = 90000000
+    worldB = 200000000
+    year = 0
+    while worldA < worldB:
+        worldA += (worldA/100)*3
+        worldB += (worldB/100)*1.5
+        year += 1
 
+    print(f'foi preciso {year} anos para o País A passar o País B')
+
+def q11():
+    numero = input("Digite um número inteiro: ")
+    algarismo = input("Digite um algarismo (0 a 9): ")
+
+    if len(algarismo) != 1 or not algarismo.isdigit():
+        print("Erro: você deve digitar um único algarismo entre 0 e 9.")
+    else:
+        if algarismo in numero:
+            print(f"O algarismo {algarismo} aparece no número {numero}.")
+        else:
+            print(f"O algarismo {algarismo} não aparece no número {numero}.")
+
+def q12():
+    char = input("Digite um caractere: ")
+
+    if len(char) != 1:
+        print("Erro: Você deve digitar apenas um único caractere.")
+    else:
+        if char.isupper():
+            print(f"O caractere '{char}' é uma letra maiúscula.")
+        elif char.islower():
+            print(f"O caractere '{char}' é uma letra minúscula.")
+        elif char.isdigit():
+            print(f"O caractere '{char}' é numérico.")
+        else:
+            print(f"O caractere '{char}' é um símbolo especial.")
